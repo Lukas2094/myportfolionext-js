238ee2586d1189189415bbd69a69bc40acd7cf69
lucassilvaativo <lucas.silva@ativo.com>
2023-05-12 17:22:07 -0300

    construção Portifolio

---
+++ src/styles/styles.ts
@@ -0,0 +1,15 @@
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { styled } from '@mui/material/styles';
+
+
+export const HeaderContainer = styled(AppBar)`
+  background-color: #000;
+`;
+
+export const HeaderToolBar = styled(Toolbar)``;
+
+export const HeaderTitle = styled(Typography)`
+  flex-grow: 1;
+`;
